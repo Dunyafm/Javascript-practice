a04ab5c1d7c2076afdd46679623b17d511d08632
unknown <Dunyafm@code.edu.az>
2022-01-24 15:08:25 -0800

    Javascript-practice

---
+++ assets/js/script.js
@@ -0,0 +1,81 @@
+//var name = "Dunya"
+//alert(name);
+
+
+
+//var, let , const
+
+
+//var groupName = "Hello World";
+//var groupName = "P126";
+//console.log(groupName);
+
+//const age = 33;
+
+//age = 55;
+
+//console.log(age);
+
+//let name;
+//let age;
+
+//console.log(age);
+
+//let name;
+//let age;
+
+//console.log(name)
+
+//for(var i=0; i < 5; i++) {
+
+//}
+
+//console.log(i)
+
+
+//let age = 33.5;
+/*
+let surname = "Esgerov";
+
+let isMarried = true;
+
+let person = {
+    name : "Dunya",
+    surname : "Mammadli",
+    age : age,
+}
+
+person.age = 55;
+person.age = 88;
+
+
+
+
+console.log(person.name + " " + person.surname + "" + person.age);
+
+console.log( "${person.name} ${person.surname}");
+
+console.log(person);
+ 
+
+let surname = null;
+
+console.log(typeof surname);
+
+*/
+let num1 = "Dunya";
+let num2 = 22;
+
+if(!isNaN(num1)){
+    console.log(num1/num2);
+
+}else{
+    console.log("Not possible")
+}
+if(num1==){
+    console.log(num1/num2);
+}else{
+    console.log("Not possible")
+}
+
+console.log(num1/num2);
